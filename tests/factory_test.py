695aa63c966629d93ccf60113375d8150299e603
David Fan <dfan14051@gmail.com>
2018-11-18 18:26:30 -0500

    Fix tests again by removing all whitespace from html

--- tests/factory_test.py
+++ tests/factory_test.py
@@ -3,7 +3,7 @@ def get_dfanme_html():
     Helper function that gets test html from my personal website
     """
     f = open("tests/dfan.me.html", "r")
-    return f.read().strip()
+    return f.read()
 
 
 def test_fetch(client):
@@ -11,7 +11,9 @@ def test_fetch(client):
     Tests fetch route using my personal website
     """
     response = client.get("/fetch/?url=dfan.me")
-    assert response.data.decode("utf-8") == get_dfanme_html()
+    assert response.data.decode("utf-8").replace("\n", "").replace(
+        " ", ""
+    ) == get_dfanme_html().replace("\n", "").replace(" ", "")
 
 
 def test_fetch_fail(client):
